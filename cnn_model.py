4c33a61236533cd4002ad1534c664d7324c53af2
Sankar Addala <95013212+SNKR004@users.noreply.github.com>
2024-08-21 14:47:48 +0530

    Create cnn_model.py

---
+++ cnn_model.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.utils import to_categorical
+
+# Load data
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0  # Normalize the images
+
+# CNN model
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(10, activation='softmax')  # 10 classes for CIFAR-10
+])
+
+# Compiling
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Training
+model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test))
+
+# Evaluating
+test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
+print(f'\nTest accuracy: {test_acc}')
+
+# Prediction
+predictions = model.predict(x_test)
+
+# Validation
+predicted_class = tf.argmax(predictions[0]).numpy()
+print(f'Predicted class for the first test image: {predicted_class}')
+
+# Save the model
+model.save('cifar10_cnn_model.keras')
